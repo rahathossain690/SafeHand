40710e55c426c669bbe88559c4316e483e473a7b
Rahat Hossain <rahathossain690@gmail.com>
2020-04-16 19:44:05 +0600

    onek kisu added

--- app/src/main/java/com/example/safehand/MainActivity.java
+++ app/src/main/java/com/example/safehand/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.otaliastudios.cameraview.CameraListener;
@@ -14,33 +16,42 @@ import com.otaliastudios.cameraview.CameraView;
 import com.otaliastudios.cameraview.PictureResult;
 import com.otaliastudios.cameraview.VideoResult;
 import com.otaliastudios.cameraview.controls.Facing;
+import com.otaliastudios.cameraview.controls.Flash;
+import com.otaliastudios.cameraview.controls.Mode;
 
 public class MainActivity extends AppCompatActivity {
+
+    public PictureResult excess;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CameraView camera = findViewById(R.id.camera);
+        final CameraView camera = findViewById(R.id.camera);
         camera.setLifecycleOwner(this);
 
-        camera.setFacing(Facing.FRONT);
+        camera.setFacing(Facing.BACK);
+        camera.setMode(Mode.PICTURE);
+        camera.setFlash(Flash.AUTO);
+        camera.setPlaySounds(true);
 
         camera.addCameraListener(new CameraListener() {
             @Override
             public void onPictureTaken(PictureResult result) {
+                excess = result;
                 Toast.makeText(MainActivity.this, "Picture taken", Toast.LENGTH_SHORT).show();
                 // A Picture was taken!
             }
+        });
 
+        ((ImageView)findViewById(R.id.capturebutton)).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onVideoTaken(VideoResult result) {
-                Toast.makeText(MainActivity.this, "Video taken", Toast.LENGTH_SHORT).show();
-                // A Video was taken!
+            public void onClick(View v) {
+                excess = null;
+                camera.takePicture();
+                //Toast.makeText(MainActivity.this, "CLICKED", Toast.LENGTH_SHORT).show();
             }
-
-            // And much more
         });
     }
 }
