a39f53945b65e6abf13e8e3c1c28ac41c7de830d
Rahat Hossain <rahathossain690@gmail.com>
2020-04-16 16:03:29 +0600

    image added

--- app/src/main/java/com/example/safehand/MainActivity.java
+++ app/src/main/java/com/example/safehand/MainActivity.java
@@ -22,29 +22,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-//        int MY_PERMISSIONS_REQUEST_CAMERA=0;
-//        // Here, this is the current activity
-//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
-//        {
-//            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA))
-//            {
-//
-//            }
-//            else
-//            {
-//                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA );
-//                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-//                // app-defined int constant. The callback method gets the
-//                // result of the request.
-//            }
-//        }
-
-
-
         CameraView camera = findViewById(R.id.camera);
         camera.setLifecycleOwner(this);
+
         camera.setFacing(Facing.FRONT);
+
         camera.addCameraListener(new CameraListener() {
             @Override
             public void onPictureTaken(PictureResult result) {
